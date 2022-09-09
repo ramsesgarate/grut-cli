790218fb0f2bb282484423a8c7af4a027f5cc65f
Ramses Garate <47763122+ramsesgarate@users.noreply.github.com>
2022-09-09 17:56:34 -0300

    feat: add messages for rut generated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import { generate, format } from '@rsgarate/rut'
+import chalk from 'chalk'
 import clipboard from 'clipboardy'
+
+const textCopy = chalk.green('It has been copied to the clipboard')
 
 function cli (flags) {
   const { withFormat } = flags
 
-  const output = withFormat ? format(generate()) : generate()
+  const rut = withFormat ? format(generate()) : generate()
 
-  clipboard.writeSync(output)
+  clipboard.writeSync(rut)
+
+  const output = `
+  Rut generated: ${chalk.blue(rut)}
+
+  ${textCopy}`
   console.log(output)
 }
+
 export default cli
